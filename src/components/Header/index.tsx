c2de3d2a83fcc7ab91acae1124d155f9f34540cf
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-11-22 21:46:53 -0300

    style: Ajustando etilização do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 export function Header() {
   return (
-    <header className="flex items-center bg-dark__principal-200 text-white text-lg p-4 w-full h-1/4 ">
+    <header className="font-montserrat font-medium flex items-center bg-dark__principal-200 text-white text-lg p-4 w-full h-1/4">
       <div className="flex justify-between w-full">
         {/* Logo */}
         <Link href="/">
@@ -20,27 +20,66 @@ export function Header() {
         <div className="flex items-center space-x-4 pr-6">
           <nav className="space-x-4">
             <Link
+              href="/"
               className="
-              hover:underline
               hover:border-b-2
             hover:border-gold__principal-400
             "
-              href="/"
             >
               Início
             </Link>
-            <Link href="/sobre">Sobre</Link>
-            <Link href="/areas-de-atuacao">Áreas de Atuação</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/faq">Faq</Link>
-            <Link href="/contacto">Contacto</Link>
+            <Link
+              href="/sobre"
+              className="
+              hover:border-b-2
+            hover:border-gold__principal-400
+            "
+            >
+              Sobre
+            </Link>
+            <Link
+              href="/areas-de-atuacao"
+              className="
+              hover:border-b-2
+            hover:border-gold__principal-400
+            "
+            >
+              Áreas de Atuação
+            </Link>
+            <Link
+              href="/blog"
+              className="
+              hover:border-b-2
+            hover:border-gold__principal-400
+            "
+            >
+              Blog
+            </Link>
+            <Link
+              href="/faq"
+              className="
+              hover:border-b-2
+            hover:border-gold__principal-400
+            "
+            >
+              Faq
+            </Link>
+            <Link
+              href="/contacto"
+              className="
+              hover:border-b-2
+            hover:border-gold__principal-400
+            "
+            >
+              Contacto
+            </Link>
           </nav>
           <a
             href="LINK_PARA_WHATSAPP"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaWhatsapp color="#A08356" size="2rem" />
+            <FaWhatsapp styles={} color='#A08356' size="2rem" />
           </a>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
+      fontFamily: {
+        sans: ['var(--font-montserrat)', 'sans-serif'],
+        serif: ['var(--font-dmtext)', 'serif']
+      },
       colors: {
         dark__principal: {
           100: 'rgba(0, 0, 0, 0.87)',
