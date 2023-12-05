414a48f06b7dc27fb8150bc2f7c17623be0dd767
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-04 21:55:48 -0300

    fix: prismic

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,69 +4,6 @@ import type * as prismic from '@prismicio/client';
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type PostsDocumentDataSlicesSlice = AlternateGridSlice;
-
-/**
- * Content for Posts documents
- */
-interface PostsDocumentData {
-  /**
-   * Slice Zone field in *Posts*
-   *
-   * - **Field Type**: Slice Zone
-   * - **Placeholder**: *None*
-   * - **API ID Path**: posts.slices[]
-   * - **Tab**: Main
-   * - **Documentation**: https://prismic.io/docs/field#slices
-   */
-  slices: prismic.SliceZone<PostsDocumentDataSlicesSlice> /**
-   * Meta Description field in *Posts*
-   *
-   * - **Field Type**: Text
-   * - **Placeholder**: A brief summary of the page
-   * - **API ID Path**: posts.meta_description
-   * - **Tab**: SEO & Metadata
-   * - **Documentation**: https://prismic.io/docs/field#key-text
-   */;
-  meta_description: prismic.KeyTextField;
-
-  /**
-   * Meta Image field in *Posts*
-   *
-   * - **Field Type**: Image
-   * - **Placeholder**: *None*
-   * - **API ID Path**: posts.meta_image
-   * - **Tab**: SEO & Metadata
-   * - **Documentation**: https://prismic.io/docs/field#image
-   */
-  meta_image: prismic.ImageField<never>;
-
-  /**
-   * Meta Title field in *Posts*
-   *
-   * - **Field Type**: Text
-   * - **Placeholder**: A title of the page used for social media and search engines
-   * - **API ID Path**: posts.meta_title
-   * - **Tab**: SEO & Metadata
-   * - **Documentation**: https://prismic.io/docs/field#key-text
-   */
-  meta_title: prismic.KeyTextField;
-}
-
-/**
- * Posts document from Prismic
- *
- * - **API ID**: `posts`
- * - **Repeatable**: `true`
- * - **Documentation**: https://prismic.io/docs/custom-types
- *
- * @typeParam Lang - Language API ID of the document.
- */
-export type PostsDocument<Lang extends string = string> =
-  prismic.PrismicDocumentWithUID<Simplify<PostsDocumentData>, 'posts', Lang>;
-
-export type AllDocumentTypes = PostsDocument;
-
 /**
  * Primary content in *AlternateGrid → Primary*
  */
@@ -257,15 +194,11 @@ declare module '@prismicio/client' {
     (
       repositoryNameOrEndpoint: string,
       options?: prismic.ClientConfig
-    ): prismic.Client<AllDocumentTypes>;
+    ): prismic.Client;
   }
 
   namespace Content {
     export type {
-      PostsDocument,
-      PostsDocumentData,
-      PostsDocumentDataSlicesSlice,
-      AllDocumentTypes,
       AlternateGridSlice,
       AlternateGridSliceDefaultPrimary,
       AlternateGridSliceDefaultItem,
